2015cc9bae6072e375ce1822710b56e1dcddb906
yi-jin <dlwls4338@naver.com>
2023-01-09 11:31:45 +0900

    3rd

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -1,13 +1,27 @@
 package com.fastcampus.springbootpractice;
 
+import com.fastcampus.springbootpractice.properties.MyProperties;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
+    private final MyProperties myProperties;
+
+    public FastcampusSpringBootPracticeApplication(MyProperties myProperties){
+        this.myProperties = myProperties;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("[configurationProps]" + myProperties.getHeight());
     }
 
 }
